9fd9c3c8007e2bfa4f5c3d06aadb42c46af7c3e7
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-02-13 14:01:09 -0800

    Add map input card to map

--- scripts/map.js
+++ scripts/map.js
@@ -1,14 +1,16 @@
-// Built with help from https://developers.google.com/maps/documentation/javascript/adding-a-google-map#maps_add_map-javascript
+// Built with help from: https://developers.google.com/maps/documentation/javascript/adding-a-google-map#maps_add_map-javascript
+// With the searching based on :https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete#maps_places_autocomplete-javascript
 
 function mapStart() {
-    const CR = {lat: 50.02554744197325, lng: -125.25163846178474};
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom:15,
-        center: CR,
+        center: {lat: 50.025, lng: -125.251},
         mapTypeControl: false,
     });
+    const cardControls = document.getElementById("controlsCard")
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(cardControls)
     const marker = new google.maps.Marker({
-        position: CR,
+        position: {lat: 50.025, lng: -125.251},
         map: map,
     });
 }
