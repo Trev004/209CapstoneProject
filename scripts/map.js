852afa6d60b649d784a935855768c0b132124724
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-02-13 10:51:36 -0800

    Add basic google map to site

---
+++ scripts/map.js
@@ -0,0 +1,15 @@
+// Built with help from https://developers.google.com/maps/documentation/javascript/adding-a-google-map#maps_add_map-javascript
+
+function mapStart() {
+    const CR = {lat: 50.02554744197325, lng: -125.25163846178474};
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom:3,
+        center: CR
+    });
+    const marker = new google.maps.Marker({
+        position: CR,
+        map: map,
+    });
+}
+
+window.mapStart = mapStart;
