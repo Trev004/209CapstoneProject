f459ea8da28a7d3e21328cd6e9aa95167ff2ceae
Trev004 <71028998+Trev004@users.noreply.github.com>
2023-02-13 14:26:32 -0800

    Add some autocomplete functionality to map
    Doesn't pan to location yet, or respect whether or not you're filtering by Address or City, but the autocomplete results show up

--- scripts/map.js
+++ scripts/map.js
@@ -7,8 +7,17 @@ function mapStart() {
         center: {lat: 50.025, lng: -125.251},
         mapTypeControl: false,
     });
-    const cardControls = document.getElementById("controlsCard")
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(cardControls)
+    const cardControls = document.getElementById("controlsCard");
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(cardControls);
+
+    const locationInput = document.getElementById("locationInput");
+    const autocomplete = new google.maps.places.Autocomplete(locationInput);
+    autocomplete.bindTo("bounds", map);
+
+    const infoWindow = new google.maps.InfoWindow();
+    const infoWindowContent = document.getElementById("autocomplete-content")
+    infoWindow.setContent(infoWindowContent)
+
     const marker = new google.maps.Marker({
         position: {lat: 50.025, lng: -125.251},
         map: map,
